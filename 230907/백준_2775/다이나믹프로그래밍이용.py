ef61bff6d5a805561c3a1e512e643776060832af
m2nseop <mini071663@gmail.com>
2023-09-07 14:38:16 +0900

    .

---
+++ 230907/백준_2775/다이나믹프로그래밍이용.py
@@ -0,0 +1,23 @@
+# 다이나믹 프로그래밍을 통한 계산
+for i in range(1, 15):
+    for j in range(1, 15):
+        apart[i][j] = apart[i][j-1] + apart[i-1][j]
+
+T = int(input())  # Test Case의 수
+
+for _ in range(T):
+    k = int(input())
+    n = int(input())
+    
+    # 아파트 크기를 동적으로 조절
+    if k >= len(apart):
+        for i in range(len(apart), k + 1):
+            row = [0] * 15
+            apart.append(row)
+    
+    # 다이나믹 프로그래밍을 통해 결과 계산
+    for i in range(1, k + 1):
+        for j in range(1, n + 1):
+            apart[i][j] = apart[i][j-1] + apart[i-1][j]
+
+    print(apart[k][n])
