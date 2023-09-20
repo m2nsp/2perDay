f8323974d3d72fa48fc9b7bb8f16387d0660807b
m2nseop <mini071663@gmail.com>
2023-09-20 15:27:06 +0900

    다시봐야함

---
+++ 230920/백준_1009.py
@@ -0,0 +1,21 @@
+# 분산처리
+
+"""
+T = int(input())
+
+def get_com(a, b):
+  data_num = a**b
+  com_num = data_num % 10
+  return com_num
+
+
+for test in range(T):
+  a,b = list(map(int, input().split()))
+  print(get_com(a,b))
+
+  
+반복문으로 구현하는 것은 시간제한이 1초이므로 시간초과 problem 발생
+"""
+
+
+# 스택으로 어케풀어......................ㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜ
