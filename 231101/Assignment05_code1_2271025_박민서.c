9ed7bef7d96dbd865bae98a6fc85c3c90e978426
m2nseop <mini071663@gmail.com>
2023-11-01 20:46:37 +0900

    .

--- 231101/Assignment05_code1_2271025_박민서.c
+++ 231101/Assignment05_code1_2271025_박민서.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-typedef struct TreeNode
-{
+typedef struct TreeNode {
 	int data;
 	struct TreeNode* left, * right, * parent;
-} TreeNode;
+}TreeNode;
+
 
 TreeNode n1 = { 'A', NULL, NULL, NULL };
 TreeNode n2 = { 'B', NULL, NULL, NULL };
@@ -15,29 +14,33 @@ TreeNode n4 = { 'D', NULL, NULL, NULL };
 TreeNode n5 = { 'E', NULL, NULL, NULL };
 TreeNode n6 = { 'F', &n4, &n5, NULL };
 TreeNode n7 = { 'G', &n3, &n6, NULL };
-TreeNode *exp_node = &n7;
+TreeNode* exp_node = &n7;
 
-TreeNode* right_most_node(TreeNode* p) {
-	if (p->right != NULL) {
-		p = p->right;
-		right_most_node(p);
+TreeNode* left_most_node(TreeNode* p) {
+	if (p->left != NULL) {
+		p = p->left;
+		left_most_node(p);
 	}
 	else
 		return p;
 }
 
-TreeNode* tree_predecessor(TreeNode* p) {
-	if (p->left != NULL)
-		return right_most_node(p->left);
+TreeNode* tree_successor(TreeNode* p) {
+	if (p->right != NULL)
+		return left_most_node(p->right);
 	TreeNode* y = p->parent;
-	while (y != NULL && p == y->left) {
+	while (y != NULL && p == y->right) {
 		p = y;
 		y = y->parent;
 	}
 	return y;
 }
 
-void main() {
+
+
+
+void main()
+{
 	TreeNode* q = exp_node;
 	n1.parent = &n3;
 	n2.parent = &n3;
@@ -46,11 +49,11 @@ void main() {
 	n5.parent = &n6;
 	n6.parent = &n7;
 	n7.parent = NULL;
-	// Go to the rightmost node
-	while (q->right) q = q->right;
-		do {
-			printf("%c\n", q->data); // Output data
-			// Call the predecessor
-			q = tree_predecessor(q);
-		} while (q); // If not null
+	while (q->left) q = q->left; // Go to the leftmost node
+	do
+	{
+		printf("%c\n", q->data); // Output data
+		// Call the successor
+		q = tree_successor(q);
+	} while (q); // If not null
 }
